c5ef77ce1c46c8d19c9b8ee10e55c8b15a6e4434
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-11 09:00:08 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,12 @@
-def hello_t
-
+def hello_t(array)
+  if block_given?
+    array.map do |name|
+      yield(name)
+    end
+    return array
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
-
